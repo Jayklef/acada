6bddacc7453bba71676a2cc56b02627bbe3a0f7a
Jayklef <jerryarhawho@gmail.com>
2023-02-25 00:20:38 +0100

    Added Swagger ui

--- src/main/java/com/jayklef/acada/AcadaApplication.java
+++ src/main/java/com/jayklef/acada/AcadaApplication.java
@@ -2,8 +2,10 @@ package com.jayklef.acada;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AcadaApplication {
 
     public static void main(String[] args) {
