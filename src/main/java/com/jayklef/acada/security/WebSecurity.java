7c6413b30e45c90c63fffc22ab66602974351e3c
Jayklef <jerryarhawho@gmail.com>
2023-06-01 14:54:50 +0100

    Configured spring security 6

--- src/main/java/com/jayklef/acada/security/WebSecurity.java
+++ src/main/java/com/jayklef/acada/security/WebSecurity.java
@@ -1,9 +1,23 @@
 package com.jayklef.acada.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests()
+                .requestMatchers("")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+        return httpSecurity.build();
+    }
 }
